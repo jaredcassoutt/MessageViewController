33c7b72289f213a7b4c3f7df49b301eff3e5efb4
Jared Cassoutt <65668106+jaredcassoutt@users.noreply.github.com>
2021-07-30 23:42:08 -0500

    Create ChatAPI.swift

---
+++ ChatAPI.swift
@@ -0,0 +1,92 @@
+//
+//  ChatAPI.swift
+//  Jared Cassoutt
+//
+//  Created by Jared Cassoutt on 5/6/21.
+//
+
+import Foundation
+
+struct ChatAPI {
+    
+    static func fetchChat(success: @escaping(_ data:ChatInformation)->(),failure:@escaping (_ error:Error)->()) {
+        let urlString = "https://www.chatURL.com/AllMessagesAPI/"
+        print(urlString)
+        //create a url
+        if let url = URL(string: urlString) {
+            //create URLSession
+            let session = URLSession(configuration: .default)
+            //give session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error ?? "error")
+                    return
+                }
+                if let safeData = data {
+                    
+                    let jsonString = String(data: safeData, encoding: String.Encoding.utf8)
+                    let parsed = jsonString!.replacingOccurrences(of: ", NaN", with: "")
+                    let jsonData = Data(parsed.utf8)
+                    print(jsonData)
+                    let decoder = JSONDecoder()
+                    do {
+                        let decodedData = try decoder.decode(ChatInformation.self, from: jsonData)
+                        print(decodedData)
+                        DispatchQueue.main.async {
+                            success(decodedData)
+                        }
+                    }
+                    catch {
+                        print(error)
+                        failure(error)
+                    }
+                }
+            }
+            //Start task
+            task.resume()
+        }
+    }
+    
+    static func sendMessage(username: String, message: String, success: @escaping(_ data:SentMessageStatus)->(),failure:@escaping (String)->()) {
+        print("usernamee: \(username)")
+        let url = URL(string: "http://www.chatURL.com/sendMessageAPI/")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "content=\(message)&username=\(username)"
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error != nil {
+                print(error ?? "error")
+                return
+            }
+            if let safeData = data {
+                // {"respsonse":"successfully registered a new user.","email":"jaredcassoutt@gmail.com","username":"jaredcassoutt"}
+                let decoder = JSONDecoder()
+                do {
+                    let decodedData = try decoder.decode(SentMessageStatus.self, from: safeData)
+                    if decodedData.status.contains("Sent message!") {
+                        DispatchQueue.main.async {
+                            success(decodedData)
+                        }
+                    }
+                    else {
+                        failure("Username could not be found. Try loging out and loging back in.")
+                    }
+                }
+                catch {
+                    DispatchQueue.main.async {
+                        print(error)
+                        failure("Cannot connect to the Network right now, check your connection and try again later!")
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+}  
